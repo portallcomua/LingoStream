40ca1f6ebc39d96435aefc62ee9ba97e9a63645c
portallcomua <41564292+portallcomua@users.noreply.github.com>
2026-06-27 23:27:31 +0300

    Create subtitle_line.dart

---
+++ lib/models/subtitle_line.dart
@@ -0,0 +1,13 @@
+class SubtitleLine {
+  final int index;
+  final Duration start;
+  final Duration end;
+  final String text;
+
+  SubtitleLine({
+    required this.index,
+    required this.start,
+    required this.end,
+    required this.text,
+  });
+}
